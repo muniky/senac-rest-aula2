f304f9cc8dea1a5469a07cb901a6b8f1bbe565f3
muniky <muniky.santos@alunos.sc.senac.br>
2023-12-07 20:04:01 -0300

    react componente com card e consulta

---
+++ aula2/src/App.js
@@ -0,0 +1,13 @@
+import Consulta from './componentes/consulta';
+import Cadastro from './componentes/cadastro';
+
+function App() {
+  return (
+    <div className="App">
+      <Cadastro/>
+      <Consulta />
+    </div>
+  );
+}
+
+export default App;
